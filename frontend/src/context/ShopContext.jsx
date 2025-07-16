da7bfd8bbb67d796094ffc4dd4d0dc32e23ccaee
Ruchi-codes04 <p.ruchika142003@gmail.com>
2025-07-16 22:05:55 +0530

    Added placeorder page

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
-import { createContext, useState, useEffect } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import { products } from '../assets/assets';
 import { toast } from "react-toastify";
+import {useNavigate} from "react-router-dom";
 
 export const ShopContext = createContext();
 
@@ -11,6 +12,7 @@ const ShopContextProvider = (props) => {
     const [search,setSearch] = useState('');
     const [showSearch,setShowSearch] = useState(false);
     const [cartItems,setCartItems] = useState({});
+    const navigate = useNavigate();
 
 
     const addToCart = async (itemId,size) => {
@@ -91,7 +93,7 @@ const ShopContextProvider = (props) => {
         products , currency, delivery_fee,
         search,setSearch,showSearch,setShowSearch,
         cartItems,addToCart,
-        getCartCount,updateQuantity,getCartAmount
+        getCartCount,updateQuantity,getCartAmount,navigate
 
     }
 
