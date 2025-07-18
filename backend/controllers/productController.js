e8c302f5fc0b3395ae01663fbe98aee81d40659c
Ruchi-codes04 <p.ruchika142003@gmail.com>
2025-07-18 14:40:20 +0530

    Rmove & get-single product logic

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -68,10 +68,29 @@ const listProducts = async (req, res) => {
 };
 
 // Function for remove product
-const removeProduct = async (req, res) => {};
+const removeProduct = async (req, res) => {
+
+  try {
+    await productModel.findByIdAndDelete(req.body.id)
+    res.json({success:true,message:"Product removed successfully"})
+  } catch (error) {
+    console.log(error)
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // Function for single product info
-const singleProduct = async (req, res) => {};
+const singleProduct = async (req, res) => {
+  try {
+    const { productId } = req.params;
+    const product = await productModel.findById(productId);
+    res.json({ success: true, product });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
 
 export { addProduct, listProducts, removeProduct, singleProduct };
 
